0664d9d3a3d8f68af19247f5002b22153c352d5d
ahzcapital <ahzcapital01@gmail.com>
2026-09-10 20:11:37 +0300

    Harden followers route decoding

--- app/u/[username]/followers/page.tsx
+++ app/u/[username]/followers/page.tsx
@@ -1,2 +1,7 @@
 import NetworkListClient from "@/components/network-list-client";
-export default async function FollowersPage({ params }: { params: Promise<{ username: string }> }) { return <NetworkListClient username={(await params).username} mode="followers" />; }
+import { decodeRouteSegment, normalizeUsername } from "@/lib/social";
+
+export default async function FollowersPage({ params }: { params: Promise<{ username: string }> }) {
+  const { username } = await params;
+  return <NetworkListClient username={normalizeUsername(decodeRouteSegment(username))} mode="followers" />;
+}
