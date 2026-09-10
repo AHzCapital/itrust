f7c02d2edd7a9d66cd9e0e16e23b5ae52fed4c3c
ahzcapital <ahzcapital01@gmail.com>
2026-09-10 20:11:43 +0300

    Harden following route decoding

--- app/u/[username]/following/page.tsx
+++ app/u/[username]/following/page.tsx
@@ -1,2 +1,7 @@
 import NetworkListClient from "@/components/network-list-client";
-export default async function FollowingPage({ params }: { params: Promise<{ username: string }> }) { return <NetworkListClient username={(await params).username} mode="following" />; }
+import { decodeRouteSegment, normalizeUsername } from "@/lib/social";
+
+export default async function FollowingPage({ params }: { params: Promise<{ username: string }> }) {
+  const { username } = await params;
+  return <NetworkListClient username={normalizeUsername(decodeRouteSegment(username))} mode="following" />;
+}
