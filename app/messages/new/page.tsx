c8cede7992c32451f88dd81ce05a8808162ffc35
ahzcapital <ahzcapital01@gmail.com>
2026-09-10 20:28:09 +0300

    Harden new message routing

--- app/messages/new/page.tsx
+++ app/messages/new/page.tsx
@@ -1,8 +1,10 @@
+import { redirect } from "next/navigation";
 import { NewConversation } from "@/components/messages-client";
 
 type Props = { searchParams: Promise<{ to?: string }> };
 
 export default async function NewMessagePage({ searchParams }: Props) {
   const { to } = await searchParams;
-  return to ? <NewConversation username={to} /> : <NewConversation username="" />;
+  if (!to) redirect("/messages");
+  return <NewConversation username={to} />;
 }
